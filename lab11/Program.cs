8b5337315e4bf7caee1ecf42d09c0d6bb694e8a0
A1lisa <aleksandra071078@icloud.com>
2026-02-27 02:13:27 +0800

    код решения задачи

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace lab11
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Runtime.Serialization.Formatters;
+
+namespace lab11
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите шестизначное число");
+            string a = Console.ReadLine();
+            if (a.Length != 6)
+            {
+                Console.WriteLine("число не шестизначное");
+                return;
+            }
+            int first = 0;
+            int last = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                first += int.Parse(a[i].ToString());
+                last += int.Parse(a[i + 3].ToString());
+            }
+            if (first == last)
+            {
+                Console.WriteLine("число счастливое");
+            }
+            else Console.WriteLine("число несчастливое");
         }
+
     }
 }
+
+
+
