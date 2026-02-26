bc19014c9977909f432d50f92b486892224d1b31
A1lisa <aleksandra071078@icloud.com>
2026-02-27 02:41:21 +0800

    автотесты

---
+++ lab11Tests/LogicTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void LuckTest()
+        {
+            var a = Logic.Compare("123123");
+            Assert.AreEqual("число счастливое", a);
+        }
+
+        [TestMethod()]
+        public void UnluckTest()
+        {
+            var a = Logic.Compare("856524");
+            Assert.AreEqual("число несчастливое", a);
+        }
+
+        [TestMethod()]
+        public void not6Test() {
+            var a = Logic.Compare("85");
+            Assert.AreEqual("число не шестизначное", a);
+        }
+     
+
+    }
+}
